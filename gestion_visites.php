aba0a888314f47d2dac11f252b2d6b5470fe5a69
Tania <tania.juspin@gmail.com>
2019-07-16 11:44:12 +0200

    front end de la page visites

--- gestion_visites.php
+++ gestion_visites.php
@@ -6,7 +6,75 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<title></title>
 </head>
-<body>
+<body class="grey">
+
+	<nav id="tiny_bar">
+		<div id="align-left">
+			<img id="btn_burger" src="images/menu_hamburger.png" height="36px" width="36px">
+			<a href="index.php"><img src="images/logo-blanc.png" height="30px" width="45px"></a>
+		</div>
+			<img src="images/menu_profil.png" height="36px" width="36px">
+	</nav>
+
+
+	  <div id="menu_hamburger" class="hidden">
+        <ul id="menu_options">
+          <a href="index.php"><li>Accueil</li></a>
+          <hr>
+          <a href="gestion_clients.php"><li>Clients</li></a>
+          <hr>
+          <a href="gestion_visites.php"><li>Visites</li></a>
+          <hr>
+          <a href="gestion_prestas.php"><li>Prestations</li></a>
+        </ul>
+    </div>
+
+    <div id="noir" class="hidden"></div>
+
+    <div id="create">
+    	<a href="#" id="create_button"><img src="images/add_circle.png">Nouvelle visite</a>
+    </div>
+    
+    <aside class="hidden">
+    	<img src="images/croix.png" id="croix2" width="40px" height="40px" class="hidden">
+    	<h3>Filtres</h3>
+    	<form action="gestion_visites.php" method="post">
+
+    		<label for="date" name="date"><h4>A partir d'une date :</h4></label>
+    		<input type="date" name="date" id="date">
+    		<h4>Payée</h4>
+    		<p><input type="radio" name="payee" id="payee_yes" value="payee_yes"><label for="payee_yes" name="payee_yes">Oui</label>
+    		<input type="radio" name="payee" id="payee_no" value="payee_no"><label for="payee_no" name="payee_no">Non</label>
+    		<input type="radio" name="payee" id="payee_ind" value="payee_ind"><label for="payee_ind" name="payee_ind">Indifférent</label></p>
+
+    		<h4>Effectuée</h4>
+    		<p><input type="radio" name="faite" id="faite_yes" value="faite_yes"><label for="faite_yes" name="faite_yes">Oui</label>
+    		<input type="radio" name="faite" id="faite_no" value="faite_no"><label for="faite_no" name="faite_no">Non</label>
+    		<input type="radio" name="faite" id="faite_ind" value="faite_ind"><label for="faite_ind" name="faite_ind">Indifférent</label></p>
+
+    		<h4>Par prestation</h4>
+
+    		<select name="choix_presta" id="choix_presta">
+    			<option value="">Choisissez la prestation</option>
+    			<option value="1">Presta 1</option>
+    			<option value="2">Presta 2</option>
+    			<option value="3">Presta 3</option>
+    			<option value="4">Presta 4</option>
+    		</select>
+
+    		<label for="nom_client" name="nom_client"><h4>Par nom du client :</h4></label>
+    		<input type="text" name="nom_client" id="nom_client"><br>
+
+    		<input type="submit" value="Valider" id="create_button" class="valider">
+
+    	</form>
+   	</aside>
+
+   	<div id="btn_float">
+   		<img src="images/tune.png" width="50px" height="50px">
+   	</div>
+
+
 
 <script type="text/javascript" src="js/main.js"></script>
 </body>
